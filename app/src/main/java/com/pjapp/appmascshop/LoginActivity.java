224f8968e3acec4bc694694db9dbdcd60feba01b
nacimjal@gmail.com <nacimjal@gmail.com>
2021-10-30 22:35:43 -0500

    Pantalla Registro de usuarios

--- app/src/main/java/com/pjapp/appmascshop/LoginActivity.java
+++ app/src/main/java/com/pjapp/appmascshop/LoginActivity.java
@@ -2,13 +2,43 @@ package com.pjapp.appmascshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button btnIniciarSesion,btnCrearCuenta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        asignarReferencias();
+
+    }
+
+    private void asignarReferencias() {
+
+        btnIniciarSesion = findViewById(R.id.btnIniciarSesion);
+        btnCrearCuenta = findViewById(R.id.btnCrearCuenta);
+
+        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnCrearCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, AccountActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
